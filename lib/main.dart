b884b13c347cf1ecfd7de91734aa014b7ab1c54a
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-04 02:49:18 +0300

    feat: create base layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Опросник Юнга',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade500),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePage();
+  }
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Опросник Юнга.'),
+      ),
+      body: const Text('Body'),
+    );
+  }
+}
